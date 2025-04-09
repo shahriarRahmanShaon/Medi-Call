ff5fd10938e7e2ab980f6416ff4a72a5f32e78bb
shaon <shahriar@scalebridger.com>
2025-04-09 14:48:01 +0600

    Initial Commit

---
+++ Medi-Call/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Medi-Call
+//
+//  Created by Shahriar Rahman Shaon on 9/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Medi-Call/Medi_CallApp.swift
@@ -0,0 +1,17 @@
+//
+//  Medi_CallApp.swift
+//  Medi-Call
+//
+//  Created by Shahriar Rahman Shaon on 9/4/25.
+//
+
+import SwiftUI
+
+@main
+struct Medi_CallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
